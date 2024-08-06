9dba0126a9f2fdd1098d612df5573a979b5c4ba0
mavinute <matheus_vinute@hotmail.com>
2024-08-06 09:07:52 -0300

    leitura de dados para o array de experiencia, erro persistindo na styles na div content

--- index.php
+++ index.php
@@ -17,13 +17,22 @@
         </div>
       </div>
       <div class="content">
-        <h3>Contato</h3>
         <div class="box-contact">
+            <h3>Contato</h3>
             <a href="https://wa.me/<?php echo $contato["whatsapp"] ?>" target="_black" ><?php echo $contato["whatsapp"] ?></a>
             <a href="<?php echo $contato["github"] ?>" target="_black" ><?php echo $contato["github"] ?></a>
         </div>
+        
         <div class="box-expir">
-          
+          <h3>Experiência</h3>
+          <?php foreach($experiencia as $exp): ?>
+            <div class="box-exp">
+              <p><strong>Empresa: </strong><?php echo $exp['empresa'] ?></p>
+              <p><strong>Cargo: </strong><?php echo $exp['cargo'] ?></p>
+              <p><strong>Inicio: </strong><?php echo $exp['inicio'] ?></p>
+              <p><strong>Termino: </strong><?php echo $exp['termino'] ?></p>
+            </div>
+          <?php endforeach; ?>
         </div>
       </div>
     </div>
